539138d3630c895449c5f35230aa78162cf5d6e8
Mohammed <mohammedsulaiman@example.com>
2025-04-11 16:01:24 +0530

    Update configuration files and remove deploy1.yml

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/syu607.github.io/",  // Base URL for GitHub Pages deployment
+  base: "/portfolio/",  // Base URL for GitHub Pages deployment
   build: {
     outDir: 'dist'
   }
